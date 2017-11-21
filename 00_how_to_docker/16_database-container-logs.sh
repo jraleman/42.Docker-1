9da493d11432cdb2b4d0e2d436116d778cff71fc
jraleman <jraleman@bendiburg.com>
2017-11-21 00:32:17 -0800

    Added database-container-logs script

---
+++ 00_how_to_docker/16_database-container-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    16_database-container-logs.sh                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/08/03 03:29:50 by jaleman           #+#    #+#              #
+#    Updated: 2017/08/03 03:29:58 by jaleman          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Include exported enviroment variables
+. "./_ENV.sh"
+
+#------------------------------------------------------------------------------#
+# 16. Look up the spawning-pool container’s logs in real time without
+# running its shell
+#------------------------------------------------------------------------------#
+
+# Prints container's logs.
+docker logs --follow $DATABASE_CONTAINER
