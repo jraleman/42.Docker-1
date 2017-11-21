0e76dadeaf515d1a5389cc93032d08d4b7969966
jraleman <jraleman@bendiburg.com>
2017-11-21 00:34:08 -0800

    Added display-containers script file

---
+++ 00_how_to_docker/17_display-containers.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    17_display-containers.sh                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jaleman <jaleman@student.42.us.org>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/08/03 03:29:50 by jaleman           #+#    #+#              #
+#    Updated: 2017/08/03 03:29:58 by jaleman          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Include exported enviroment variables
+. "./_ENV.sh"
+
+#------------------------------------------------------------------------------#
+# 17. Display all the currently active containers on the Char virtual machine
+#------------------------------------------------------------------------------#
+
+# Display all the currently active containers.
+docker ps
