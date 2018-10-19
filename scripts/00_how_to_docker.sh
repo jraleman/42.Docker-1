33a7a8c9d648dc250077acecd21b8c0159150d3d
jraleman <jraleman@bendiburg.com>
2018-10-19 02:35:07 -0700

    Create array list of script files description

--- scripts/00_how_to_docker.sh
+++ scripts/00_how_to_docker.sh
@@ -12,7 +12,7 @@
 # **************************************************************************** #
 
 # Include exported enviroment variables
-. "./_ENV.sh"
+. "00_how_to_docker/_vars"
 
 # wtf is this?
 shopt -s nullglob
@@ -21,76 +21,64 @@ shopt -s nullglob
 declare n=0
 declare prefix=0
 # declare filenames=(*)
-declare filenames=(../00_how_to_docker/*)
-
-
-# declare description=(*) <- define list array
-
-# Pre-setup
-# Install required dependencies
-
-# Machines
-# Create a new docker machine
-# Get IP of a machine
-# Define variables
-
-# Containers
-# Create a new pull container
-# Run a container
-# Create nginx container
-# Get the IP address of a container
-# Launch Alpine Linux container
-# Launch Debian container
-
-# Volumes
-# Create volumes
-# List volumes
-
-# Container types
-# MySQL Container
-# Print container variables
-# Wordpress Container
-# PHPMyAdmin container
-
-# Container operations
-# Database logs
-# Display container
-# Restart container
-
-# Swarms
-# Create swarm
-# Create a second Docker machine
-# Join swarm
-# Overlay network
-
-# Services
-# Create service
-# List services
-# Service replicas
-# Service logs
-# A second service replica
-# Service task
-# Service scale
-
-# Delete
-# Service delete
-# Containers delete
-# Images Delete
-# Machine delete
+declare filenames=("00_how_to_docker/*")
+declare -a array=(
+                  # Pre-setup
+                  "Install required dependencies"
+                  # Machines
+                  "Create a new docker machine"
+                  "Get IP of a machine"
+                  "Define variables"
+                  # Containers
+                  "Create a new pull container"
+                  "Run a container"
+                  "Create nginx container"
+                  "Get the IP address of a container"
+                  "Launch Alpine Linux container"
+                  "Launch Debian container"
+                  # Volumes
+                  "Create volumes"
+                  "List volumes"
+                  # Container types
+                  "MySQL Container"
+                  "Print container variables"
+                  "Wordpress Container"
+                  "PHPMyAdmin container"
+                  "Container operations"
+                  "Database logs"
+                  "Display container"
+                  "Restart container"
+                  # Swarms
+                  "Create swarm"
+                  "Create a second Docker machine"
+                  "Join swarm"
+                  "Overlay network"
+                  # Services
+                  "Create service"
+                  "List services"
+                  "Service replicas"
+                  "Service logs"
+                  "A second service replica"
+                  "Service task"
+                  "Service scale"
+                  # Delete
+                  "Service delete"
+                  "Containers delete"
+                  "Images Delete"
+                  "Machine delete"
+                  )
 
 declare count=$(ls -l | grep ^- | wc -l)
 
 while [[ $n -le $count ]]; do
   prefix="${filenames[$n]:0:2}"
-#  if [[ $prefix =~ [1-9]+ ]]; then
   if [[ $prefix =~ [0-9]+ ]]; then
     echo "Running     :" ${filenames[$n]}
-    echo "Description :" ${descriptionList[$n]}
+    echo "Description :" ${array[$n]}
     echo "..."
     . ${filenames[$n]}
 
     # Locate the files
-    # ../ ${filenames[$n]}
     echo "========================================\n"
   fi
   n=$(( n+1 ))
