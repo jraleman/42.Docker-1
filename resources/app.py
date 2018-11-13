d7a08dcadc590319d9baad441ad3abad406c83be
jraleman <jraleman@bendiburg.com>
2018-11-12 22:36:30 -0800

    Add resources/app.py

---
+++ resources/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return ('<!DOCTYPE html> \
+             <html> \
+               <head> \
+                 <title>Hello World!</title> \
+               </head> \
+               <body> \
+                 <h1>Hello World</h1> \
+               </body> \
+             </html>')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="3000")
